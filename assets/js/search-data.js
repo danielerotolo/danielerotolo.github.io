ce549ae1a7d37bc2509b7afbbdd433c22d9d254a
danielerotolo <danielerotolo@gmail.com>
2026-04-30 07:17:59 +0000

    Deploying to gh-pages from @ danielerotolo/danielerotolo.github.io@d87b9c5fb357e105ed2a084acd09c4d153bc1015 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,9 +440,9 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-ugreens",
+          title: 'UGREENS',
+          description: "University for the GREEN and Sustainable transition",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
